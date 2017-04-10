ee09fcb594b976ff1aba68add36417413ad484e2
ShevtsovAS <hack85sh@gmail.com>
2017-04-11 01:40:03 +0400

    HW03 task_3_4 JdbcMealRepositoryImpl

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -29,7 +29,7 @@ CREATE TABLE meals
 (
   id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
   user_id     INTEGER NOT NULL,
-  dateTime    TIMESTAMP NOT NULL,
+  date_time   TIMESTAMP NOT NULL,
   description VARCHAR NOT NULL,
   calories    INTEGER NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
